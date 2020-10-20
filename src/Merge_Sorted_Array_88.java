c0242160ad170fb483a22a670f352195a28c008f
Hanson Nguyen <hansonn00@gmail.com>
2020-10-20 04:43:54 -0400

    merge sort

---
+++ src/Merge_Sorted_Array_88.java
@@ -0,0 +1,21 @@
+package src;
+
+import java.util.Arrays;
+
+public class Merge_Sorted_Array_88 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int j = 0;      
+        
+        for (int i = m; i < m + n; i++) {
+            if (j < n) {
+                nums1[i] = nums2[j];
+                j++;
+            }
+
+        }
+        
+        Arrays.sort(nums1);
+
+    }
+}
