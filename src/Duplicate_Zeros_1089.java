a828026ea78f5d6e590334343eab6177878f367a
Hanson Nguyen <hansonn00@gmail.com>
2020-10-20 04:43:50 -0400

    dupe zeros

---
+++ src/Duplicate_Zeros_1089.java
@@ -0,0 +1,30 @@
+package src;
+
+public class Duplicate_Zeros_1089 {
+    public void duplicateZeros(int[] arr) {
+        
+        int[] temp = new int[arr.length];
+        
+        int i = 0;
+        int j = 0;
+        
+        for (i = 0; i < arr.length; i++) {
+            if (j < arr.length) {
+                if (arr[i] == 0 && j < arr.length - 1) {
+                    temp[j] = 0;
+                    temp [j+1] = 0;
+                    j = j + 2;
+                }
+                else {
+                    temp[j] = arr[i];
+                    j++;
+                }
+            }
+        }
+        
+        for (i = 0; i < arr.length; i++) {
+            arr[i] = temp[i];
+        }
+        
+    }
+}
