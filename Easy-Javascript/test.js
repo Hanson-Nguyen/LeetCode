98738a1f1c3be7718ff885332f1a57c7b9406769
Hanson Nguyen <hansonn00@gmail.com>
2021-09-30 00:00:59 -0400

    Update test.js

--- Easy-Javascript/test.js
+++ Easy-Javascript/test.js
@@ -1,3 +1,4 @@
+/*
 arr = [1,2,3,4,5]
 for (var i = 0; i < arr.length; i++) {
     setTimeout(function (j) {
@@ -6,4 +7,20 @@ for (var i = 0; i < arr.length; i++) {
         }
     } (i), 3000);
 }
+*/
+
+
+function factorialize(num) {
+
+    let result = 0;
+
+    for (var i = 1; i <= num; i++) {
+        console.log(i);
+    }
+
+    return result;
+    
+}
+
+factorialize(5);
 
